7c9ae8cda5fb4a635371cc366b332fc90de7feb8
faleialina <august3@mail.ru>
2024-10-27 23:22:26 +0300

    adding redirect to contacts

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -23,6 +23,12 @@ const animation = {
 };
 
 export default function About() {
+    const handleContactClick = () => {
+        const footer = document.getElementById('footer');
+        if (footer) {
+            footer.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
     return <>
         <motion.div
             initial='hiddenHorizontal'
@@ -32,7 +38,9 @@ export default function About() {
             <div className={style.info}>
                 <motion.h2 custom={1} variants={animation}>Свяжитесь со мной</motion.h2>
                 <motion.p custom={2} variants={animation}>Я мотивированный и постоянно развивающийся фронтенд-разработчик. Не стесняйтесь заглянуть в мое портфолио, чтобы ознакомиться с разнообразными проектами, которые я завершила. Если Вас заинтересовала моя работа и Вы хотите, чтобы я воплотила Ваш проект в жизнь, не стесняйтесь связаться со мной!</motion.p>
-                <motion.button custom={3} variants={animation}>Мои контакты</motion.button>
+                <motion.button custom={3} variants={animation} onClick={handleContactClick}>
+                    Мои контакты
+                </motion.button>
 
             </div>
         </motion.div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import style from './footer.module.css'
 
 export default function Footer() {
-    return <footer className={style.wrapper}>
+    return <footer id="footer" className={style.wrapper}>
         <p className={style.title}>Мои контакты</p>
         <div className={style.line}></div>
         <nav className={style.icons_contacts}>
