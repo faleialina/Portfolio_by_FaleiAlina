d8352dab5edfe0b60de1327d7878d58ec5b8d5ed
faleialina <august3@mail.ru>
2024-11-21 21:57:53 +0300

    fix MeteorTrail

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import MeteorTrail from './components/MeteorTrail/MeteorTrail.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		{/* <div
-			className='App'
-			style={{ height: '100vh', position: 'relative', overflow: 'hidden' }}
-		> */}
-			<MeteorTrail />
-		{/* </div> */}
+		<MeteorTrail />
 		<App />
 	</StrictMode>
 )
